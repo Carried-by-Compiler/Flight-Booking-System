ee3c7e19f0c72148d8ff8b438ffc170f6d54d4c2
John Rey Juele <15167798@studentmail.ul.ie>
2018-10-30 22:57:09 +0000

    Can someone give me a hug? <3

--- Flight-Booking-System/src/DB_Layer/FlightsDAO.java
+++ Flight-Booking-System/src/DB_Layer/FlightsDAO.java
@@ -5,10 +5,38 @@
  */
 package DB_Layer;
 
+import business_layer.Flight;
+
 /**
  *
  * @author John Rey Juele
  */
 public class FlightsDAO implements Dao<Flight>{
+    
+    private String filename;
+    
+    public FlightsDAO() {
+        this.filename = "./Database/Flights.csv";
+    }
+
+    @Override
+    public Flight get(int id) {
+        
+    }
+
+    @Override
+    public void add(Flight n) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
